0c7277aa2d4a721aca979959ae600af250af4576
Giap Nguyen <giap9416@Giaps-MacBook-Pro.local>
2020-10-14 07:41:13 +0700

    add confiig

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.get('*', (req, res) => {                       
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(port, () => {
    console.log('Server is up!');
 });
